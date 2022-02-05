0c3684cc3e4614796e95af2a9c013408a2cecaa6
Kevin <kevinlamaster@gmail.com>
2022-02-05 13:20:11 -0600

    Added Jesus Animation

--- Assets/Scripts/Pets/Jesus.cs
+++ Assets/Scripts/Pets/Jesus.cs
@@ -35,6 +35,9 @@ public class Jesus : Pets
     {
         this.setATK(this.getATK() + 2);
         this.setHP(this.getHP() + 2);
+        GameObject cross = Resources.Load("UI/Cross") as GameObject;
+        BattleHandler.instance.spawnObjectAbove(cross, this.visualEffect);
+
     }
 
 }
